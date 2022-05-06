29e0f47683c3708b188605c8cfb0fb58c4e1ce77
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-05-06 15:33:10 +0200

    Add ghost script and prefab

---
+++ src/PacMan/Assets/Scripts/GhostBehaviour.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+enum Direction
+{
+    Horizontal,
+    Vertical
+}
+
+public class GhostBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Direction direction;
+
+    private new Rigidbody2D rigidbody2D;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        SetStartingVelocity();
+    }
+
+    private void Update()
+    {
+        SetSpriteDirection();
+    }
+
+    private void SetStartingVelocity()
+    {
+        switch (direction)
+        {
+            case Direction.Horizontal:
+                rigidbody2D.velocity = Vector2.right * speed;
+                break;
+            case Direction.Vertical:
+                rigidbody2D.velocity = Vector2.up * speed;
+                break;
+        }
+    }
+
+    private void SetSpriteDirection()
+    {
+        if (rigidbody2D.velocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+            return;
+        }
+
+        spriteRenderer.flipX = false;
+    }
+}
