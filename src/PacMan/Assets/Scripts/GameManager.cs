8bd8e91a75dc5a5847cbfc85572cda31d9d79d54
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-05-06 14:10:30 +0200

    Make pellets work

--- src/PacMan/Assets/Scripts/GameManager.cs
+++ src/PacMan/Assets/Scripts/GameManager.cs
@@ -2,32 +2,15 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
-    private Transform cherries;
-
-    public static int Score;
-
-    public static int RemainingCherries { get; private set; }
+    private static int score;
 
     private void Start()
     {
-        NewGame();
+        score = 0;
     }
 
-    private void NewGame()
+    public static void IncreaseScore(int increaseAmount)
     {
-        Score = 0;
-
-        RemainingCherries = cherries.childCount;
-    }
-
-    public static void DecreaseCherryCount(int decreaseAmount = 1)
-    {
-        RemainingCherries -= decreaseAmount;
-    }
-
-    public static bool CheckCherryCount()
-    {
-        return RemainingCherries <= 0 ? true : false;
+        score += increaseAmount;
     }
 }
--- src/PacMan/Assets/Scripts/PelletBehaviour.cs
+++ src/PacMan/Assets/Scripts/PelletBehaviour.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class PelletBehaviour : MonoBehaviour
 {
+    private const int POINTS_VALUE = 1;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        gameObject.SetActive(false);
+        HandlePacCollision(collision);
+    }
 
-        GameManager.Score++;
+    private void HandlePacCollision(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pac"))
+        {
+            gameObject.SetActive(false);
+
+            GameManager.IncreaseScore(POINTS_VALUE);
+        }
     }
 }
