1bae16d6520247b73286181adcc037c59b207357
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-05-06 14:34:19 +0200

    Add cherry script

---
+++ src/PacMan/Assets/Scripts/CherryBehaviour.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CherryBehaviour : MonoBehaviour
+{
+    private const int POINTS_VALUE = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandlePacCollision(collision);
+    }
+
+    private void HandlePacCollision(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pac"))
+        {
+            gameObject.SetActive(false);
+
+            GameManager.IncreaseScore(POINTS_VALUE);
+
+            GameManager.DecreaseCherryCount();
+        }
+    }
+}
--- src/PacMan/Assets/Scripts/GameManager.cs
+++ src/PacMan/Assets/Scripts/GameManager.cs
@@ -4,13 +4,25 @@ public class GameManager : MonoBehaviour
 {
     private static int score;
 
+    private static int cherriesRemaining;
+
+    [SerializeField]
+    private Transform[] cherries;
+
     private void Start()
     {
         score = 0;
+
+        cherriesRemaining = cherries.Length;
     }
 
     public static void IncreaseScore(int increaseAmount)
     {
         score += increaseAmount;
+    }
+
+    public static void DecreaseCherryCount()
+    {
+        cherriesRemaining--;
     }
 }
