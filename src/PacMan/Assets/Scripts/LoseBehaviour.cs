388fa1a89ce7bbd6ae263842dd9c6fa430e6a455
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-05-08 13:30:52 +0200

    Update LoseBehaviour.cs

--- src/PacMan/Assets/Scripts/LoseBehaviour.cs
+++ src/PacMan/Assets/Scripts/LoseBehaviour.cs
@@ -1,17 +1,37 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoseBehaviour : MonoBehaviour
 {
     private static TextMeshProUGUI text;
 
+    private static bool roundOver;
+
     private void Awake()
     {
         text = GetComponent<TextMeshProUGUI>();
+
+        roundOver = false;
+    }
+
+    private void Update()
+    {
+        ReloadScene();
     }
 
     public static void EnableLoseText()
     {
         text.enabled = true;
+
+        roundOver = true;
+    }
+
+    private void ReloadScene()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && roundOver)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
