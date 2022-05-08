f4cd6559f173f665cf0d56dd5cce432d37c21bbd
devSomethingSomething <61376074+devSomethingSomething@users.noreply.github.com>
2022-05-08 15:30:58 +0200

    Improve door script

--- src/PacMan/Assets/Scripts/GameManager.cs
+++ src/PacMan/Assets/Scripts/GameManager.cs
@@ -9,11 +9,22 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Transform cherries;
 
+    [SerializeField]
+    private Collider2D door;
+
     private void Start()
     {
         score = 0;
 
         cherriesRemaining = cherries.childCount;
+    }
+
+    private void Update()
+    {
+        if (cherriesRemaining <= 0)
+        {
+            door.isTrigger = true;
+        }
     }
 
     public static void IncreaseScore(int increaseAmount)
